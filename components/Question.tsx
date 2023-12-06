c125bd10356a15d5676a20c630584190e85ee510
Gabe Jimenez <jimenezgabriel12@gmail.com>
2023-12-05 18:08:18 -0700

    Create 'Question' component to ask general questions about journal entries and add ai functionality to it

---
+++ components/Question.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { askQuestion } from "@/utils/api";
+import { useState } from "react";
+
+const Question = () => {
+  const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState();
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const answer = await askQuestion(value);
+
+    setResponse(answer);
+    setValue("");
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          disabled={loading}
+          type="text"
+          className="border border-black/20 px-4 py-2 text-lg rounded-lg"
+          value={value}
+          onChange={onChange}
+          placeholder="Ask a question"
+        />
+        <button
+          type="submit"
+          className="bg-blue-400 px-4 py-2 rounded-lg text-lg"
+        >
+          Ask
+        </button>
+      </form>
+      {loading && <div>...loading</div>}
+      {response && <div>{response}</div>}
+    </div>
+  );
+};
+
+export default Question;
