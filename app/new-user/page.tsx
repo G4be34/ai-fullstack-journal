7a0d552e3a266fecdb2b65c2d57663c883354287
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-01-03 19:55:50 -0700

    Add new icon/logo and fix breaking bug when user would try to log in with Google without already having an account

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -3,23 +3,27 @@ import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const createNewUser = async () => {
-  const user = await currentUser();
+  try {
+    const user = await currentUser();
 
-  const match = await prisma.user.findUnique({
-    where: {
-      clerkId: user.id as string,
-    },
-  });
-
-  if (!match) {
-    await prisma.user.create({
-      data: {
-        clerkId: user.id,
-        email: user?.emailAddresses[0].emailAddress,
+    const match = await prisma.user.findUnique({
+      where: {
+        clerkId: user.id as string,
       },
     });
-  }
 
+    if (!match) {
+      await prisma.user.create({
+        data: {
+          clerkId: user.id,
+          email: user?.emailAddresses[0].emailAddress,
+        },
+      });
+    }
+  } catch (error) {
+    console.error("Error creating new user:", error);
+    throw new Error(error);
+  }
   redirect("/journal");
 };
 
