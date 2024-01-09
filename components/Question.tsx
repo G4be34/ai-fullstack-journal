8843ff4e8f2363d520ec386af0cbfff4a6e6184e
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-01-08 23:53:03 -0700

    Update responsive style to input box

--- components/Question.tsx
+++ components/Question.tsx
@@ -33,7 +33,7 @@ const Question = () => {
         <input
           disabled={loading}
           type="text"
-          className="border border-black/20 px-4 py-2 text-lg rounded-lg mr-4 max-w-[550px]"
+          className="border border-black/20 px-4 py-2 text-lg rounded-lg mr-4 md:w-[350px] lg:w-[550px]"
           value={value}
           onChange={onChange}
           placeholder="Ask a question"
